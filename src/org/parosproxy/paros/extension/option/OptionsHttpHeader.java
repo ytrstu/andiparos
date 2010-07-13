35383151040bd843b8c45dc22ed495c13ce33c93
a.c.neumann <a.c.neumann@2415288e-faa6-4d0a-e47c-2266d0246679>
2010-07-13 07:55:53 +0000

    Corrected license aggreement of the new classes

--- src/org/parosproxy/paros/extension/option/OptionsHttpHeader.java
+++ src/org/parosproxy/paros/extension/option/OptionsHttpHeader.java
@@ -1,22 +1,19 @@
 /*
- *
- * Paros and its related class files.
+ * Copyright (C) 2010, Compass Security AG
  * 
- * Paros is an HTTP/HTTPS proxy for assessing web application security.
- * Copyright (C) 2003-2004 Chinotec Technologies Company
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the Clarified Artistic License
- * as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * Clarified Artistic License for more details.
+ * GNU General Public License for more details.
  * 
- * You should have received a copy of the Clarified Artistic License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/copyleft/
+ * 
  */
 
 package org.parosproxy.paros.extension.option;
