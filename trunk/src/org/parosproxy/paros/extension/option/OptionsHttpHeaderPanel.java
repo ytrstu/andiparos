5f9dec8505fcbaaacdd4ce65a800e79876b2becc
a.c.neumann <a.c.neumann@2415288e-faa6-4d0a-e47c-2266d0246679>
2010-07-15 18:33:11 +0000

    Copyright corrections

--- trunk/src/org/parosproxy/paros/extension/option/OptionsHttpHeaderPanel.java
+++ trunk/src/org/parosproxy/paros/extension/option/OptionsHttpHeaderPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Compass Security AG
+ * Copyright (C) 2010, Andiparos Project, Axel Neumann
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
